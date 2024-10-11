d8f80bd8f18d1a1fd1ad130ef934cf13611867fe
salvius <salvius.stephen@gmail.com>
2024-10-11 09:08:11 +0200

    move code10

---
+++ tes.go
@@ -0,0 +1,126 @@
+package nats
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	"github.com/dop251/goja"
+	natsio "github.com/nats-io/nats.go"
+	"go.k6.io/k6/js/common"
+	"go.k6.io/k6/js/modules"
+)
+
+func init() {
+	modules.Register("k6/x/nats", new(RootModule))
+}
+
+// RootModule is the global module object type. It is instantiated once per test
+// run and will be used to create k6/x/nats module instances for each VU.
+type RootModule struct{}
+
+// ModuleInstance represents an instance of the module for every VU.
+type Nats struct {
+	conn    *natsio.Conn
+	vu      modules.VU
+	exports map[string]interface{}
+}
+
+// Ensure the interfaces are implemented correctly.
+var (
+	_ modules.Instance = &Nats{}
+	_ modules.Module   = &RootModule{}
+)
+
+// NewModuleInstance implements the modules.Module interface and returns
+// a new instance for each VU.
+func (r *RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	mi := &Nats{
+		vu:      vu,
+		exports: make(map[string]interface{}),
+	}
+
+	mi.exports["Nats"] = mi.client
+
+	return mi
+}
+
+// Exports implements the modules.Instance interface and returns the exports
+// of the JS module.
+func (mi *Nats) Exports() modules.Exports {
+	return modules.Exports{
+		Named: mi.exports,
+	}
+}
+
+func (n *Nats) client(c goja.ConstructorCall) *goja.Object {
+	rt := n.vu.Runtime()
+
+	var cfg Configuration
+	err := rt.ExportTo(c.Argument(0), &cfg)
+	if err != nil {
+		common.Throw(rt, fmt.Errorf("Nats constructor expect Configuration as it's argument: %w", err))
+	}
+
+	natsOptions := natsio.GetDefaultOptions()
+	natsOptions.Servers = cfg.Servers
+	if cfg.Unsafe {
+		natsOptions.TLSConfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	}
+	if cfg.Token != "" {
+		natsOptions.Token = cfg.Token
+	}
+
+	conn, err := natsOptions.Connect()
+	if err != nil {
+		common.Throw(rt, err)
+	}
+
+	return rt.ToValue(&Nats{
+		vu:   n.vu,
+		conn: conn,
+	}).ToObject(rt)
+}
+
+func (n *Nats) Close() {
+	if n.conn != nil {
+		n.conn.Close()
+	}
+}
+
+
+func (n *Nats) main() error {
+	if n.conn == nil {
+		return fmt.Errorf("the connection is not valid")
+	}
+
+	js, err := n.conn.JetStream()
+	if err != nil {
+		return fmt.Errorf("cannot accquire jetstream context %w", err)
+	}
+
+	_, err = js.Publish("", []byte("Hello from test file"))
+
+	return err
+}
+
+type Configuration struct {
+	Servers []string
+	Unsafe  bool
+	Token   string
+}
+
+type Message struct {
+	Raw    []byte
+	Data   string
+	Topic  string
+	Header map[string]string
+}
+
+type Subscription struct {
+	Close func() error
+}
+
+type MessageHandler func(Message)
