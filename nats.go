d54fde956180a2d968638632dfa7dd41250de286
salvius <salvius.stephen@gmail.com>
2024-10-16 17:12:01 +0200

    move code39

--- nats.go
+++ nats.go
@@ -103,7 +103,7 @@ func (n *Nats) Subscribe(topic string, handler MessageHandler) (*Subscription, e
 		// }
 
 		message := Message{
-//			Raw:    msg.Data,
+			Raw:    msg.Data,
 			Data:   Ack(),
 			Topic:  msg.Subject,
 //			Header: h,
@@ -133,7 +133,7 @@ type Configuration struct {
 }
 
 type Message struct {
-//	Raw    []byte
+	Raw    []byte
 	Data   Ack()
 	Topic  string
 //	Header map[string]string
